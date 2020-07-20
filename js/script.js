4922a4de82b76e3a7afe7d93552097689ed25300
Tracy Chen <traucyy@gmail.com>
2020-07-20 00:35:22 -0700

    Added new files

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//hide header on scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+//blurry header
+var pageContent = document.getElementById("content"),
+	pagecopy = pageContent.cloneNode(true),
+	blurryContent = document.getElementById("blurryscroll");
+	blurryContent.appendChild(pagecopy);
+	window.onscroll = function() { blurryContent.scrollTop = window.pageYOffset; }
+
+
+// https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_input_test
+//copy to clipboard
+function myFunction() {
+  var copyText = document.getElementById("myEmail");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied! &#128571;";
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy to clipboard &#128570;";
+}
+
+
+
+
+//spacer
